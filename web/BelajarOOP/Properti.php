a8cf33d6eb02c4089fe0feaddf3a7a4242e6334a
ozoana <ahmadfirdaus2707@gmail.com>
2025-09-17 10:04:34 +0800

    commit ulang

--- web/BelajarOOP/Properti.php
+++ web/BelajarOOP/Properti.php
@@ -7,9 +7,42 @@
 //namun ditempatkan di dalam class dan harus di awali kata kuncti "var"
 //jadi jika variable kan harus tanda $ nah jika properti ini harus var
 
-class Person
+class person
 {
     var $name;
     var $address;
     var $country;
+}
+
+//di properti ini kita bisa menambahkan type declaration
+//type declaration itu seperti menetukan itu int, float dan string DLL
+//contoh : 
+
+class sekolah
+{
+    var string $nama;
+    var string $jurusan;
+    var int $kelas;
+}
+
+//default properti value
+//sama seperti  variable, di properti juga bisa langsung mengisi value nya 
+//jadi jika value nya tidak di ubah, dia akan menggunakan velue default yang sudah di tuntukan
+//contoh :
+
+class car {
+    var string $name;
+    var string $color = "merah";
+    var float $harga;
+} 
+
+//nullable properti
+//saat kita menambahkan declarasion di properti ataau di function argument
+//maka secara otomatis kita tidak bisa mengirim data null kedalam properti atau function argument 
+//di PHP 7.4 keataas dikenalkan yang nama nya nullable type, jadi kita bisa mmengirim data null ke properti atau function argument
+//cara memggunakan nya sebelum typr declarasion diberikan tanda tanya
+//contoh:
+class rumah {
+    var string $tempat;
+    var ?string $alamat = null;
 }
--- web/BelajarOOP/objectUseproperti.php
+++ web/BelajarOOP/objectUseproperti.php
@@ -12,7 +12,55 @@ $person1->country = "indonesia";
 
 // nah itu untuk mengisi isi data nya.
 //nah jika ini untuk menampilkan data nya
-
+echo "\n";
 echo "name : {$person1->name}\n";
 echo "address : {$person1->address}\n";
 echo "country : {$person1->country}\n";
+
+//properti menggunakan type declaration :
+$siswa = new sekolah();
+$siswa ->nama = "daus";
+$siswa ->jurusan = "RPL";
+$siswa ->kelas = 12; //misal di isi dengan string "12" juga benar, karena php memiliki type juggling
+
+echo "\n";
+echo "ini contoh menggunakan type declaration : \n";
+echo "nama : {$siswa->nama}\n";
+echo "jurusan : {$siswa->jurusan}\n";
+echo "kelas : {$siswa->kelas}\n";
+
+//properti menggunakan value default :
+echo "\n";
+echo "ini contoh menggunakan value default : \n";
+
+$mobil = new car();
+$mobil ->name = "avanza";
+$mobil ->harga = 1.1;
+
+echo "name : {$mobil->name}\n";
+echo "color : {$mobil->color}\n";
+echo "harga : {$mobil->harga}\n"; 
+
+//ini jika diberikan value default tapi isi nya di ubah, jadi dia menggunakan data terbaru, bukan default
+$mobil2 = new car();
+$mobil2 ->name = "Supra";
+$mobil2 ->color = "hitam";
+$mobil2 ->harga = 5.7;
+
+echo "\n";
+echo "ini contoh value default tapi menggunakan data yang di berikan, bukan default:\n";
+echo "name : {$mobil2->name}\n";
+echo "color : {$mobil2->color}\n";
+echo "harga : {$mobil2->harga}\n"; 
+
+//contoh properti menggunakan nullable :
+echo "\n";
+echo "ini contoh menggunakan type nullable : \n";
+
+$home = new rumah();
+$home ->tempat = "kalimantan";
+$home ->alamat = null;//karena di set nullable
+
+echo "tempat : {$home -> tempat}\n";
+echo "alamat : {$home -> alamat}\n";
+
