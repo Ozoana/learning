a62f4caec8f643c18c3037ee9fb7514dd48f8ca3
ozoana <ahmadfirdaus2707@gmail.com>
2025-10-20 10:39:18 +0800

    mulai belajar javaScript

---
+++ web/JavaScript/comen.js
@@ -0,0 +1,20 @@
+//cara menambahkan komentar di javascript
+//ada dua cara
+
+//cara pertama dengan menggunakan garis-garis seperti "//" tapi koemn yang ini cuman untuk 1 baris
+
+//jika ingin banyak, kita bisa menggunakan garis lalu bintang, seperti ini : /* (isi komen)*/
+
+/*
+dan ini contoh nya jika menggunakan garis dan bintang
+jadi apapun isi nya
+asal berada di dalam area ini, maka akan di anggap comen
+*/
+
+// document.writeln("Hello word")
+
+/*  
+document.writeln("Hello word")
+ */
+
+ document.writeln("Hello word")
